598bcd6400e151bbe73a8ffdd289436c6b13eeaf
theaud <brionmathieu@gmail.com>
2016-02-19 14:19:44 +0100

    first commit

---
+++ src/tp4_exception/TP4_exception.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp4_exception;
+
+/**
+ *
+ * @author mathieu
+ */
+public class TP4_exception {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+        
+// TODO code application logic here
+    }
+    
+}
